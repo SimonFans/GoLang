84eab822aee8a61cec5d1f34ca95378eb3356817
ximengzhao <ximengzhao@microsoft.com>
2022-10-17 10:42:14 -0700

    change monster_test.go

--- practice/example/test/monster/monster_test.go
+++ practice/example/test/monster/monster_test.go
@@ -2,6 +2,14 @@ package monster
 
 import "testing"
 
+/*
+Test command:
+1. Go to path /home/xmzhao/mygit/GoLang/practice/example/test/monster
+2. Run go test -v. This will run all test files
+  2.1 If want to test only one test file, then you can run go test -v monster_test.go monster.go
+  2.2 If want to test only one function, then you can run
+*/
+
 func TestStore(t *testing.T) {
 	m1 := &Monster{
 		Name:  "WuKong",
