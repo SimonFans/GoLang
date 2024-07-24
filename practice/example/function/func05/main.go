069b09f1dd517f62cf9c98462235067aab37949d
Simon Zhao <ximengzhao1220@gmail.com>
2024-07-23 19:59:16 -0700

    a

--- practice/example/function/func05/main.go
+++ practice/example/function/func05/main.go
@@ -1,1 +1,29 @@
-package func05
+package main
+
+import "fmt"
+
+type T func(int) int
+
+func transformNumber(numbers *[]int, transform T) []int {
+	res := make([]int, 0)
+	for _, val := range *numbers {
+		res = append(res, transform(val))
+	}
+	return res
+}
+
+func doubleNumbers(number int) int {
+	return number * 2
+}
+
+func tripleNumbers(number int) int {
+	return number * 3
+}
+
+func main() {
+	input := []int{1, 2, 3, 4}
+	double := transformNumber(&input, doubleNumbers)
+	triple := transformNumber(&input, tripleNumbers)
+	fmt.Println(double)
+	fmt.Println(triple)
+}
