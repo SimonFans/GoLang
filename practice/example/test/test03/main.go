7637e0bf8a8cc8b030278b0400df4daf37029a2c
ximeng zhao <ximengzhao@microsoft.com>
2025-01-23 06:10:22 +0000

    add a func slice to use

--- practice/example/test/test03/main.go
+++ practice/example/test/test03/main.go
@@ -15,6 +15,14 @@ func (c Caculator) multiply() int {
 	return c.a * c.b
 }
 
+func adder(a, b int) int {
+	return a + b
+}
+
+func adder2(a, b int) int {
+	return a - b
+}
+
 func main() {
 	cal := Caculator{1, 3}
 	{
@@ -27,4 +35,11 @@ func main() {
 		y := cal.multiply()
 		fmt.Println(x, y)
 	}
+
+	f := []func(a, b int) int{adder, adder2}
+	for _, fn := range f {
+		f1 := fn(1, 2)
+		fmt.Println(f1)
+	}
+	fmt.Println(f)
 }
